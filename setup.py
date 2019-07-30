c69854550c9d913e52f0b9257ce75883b68a3483
Sintax <sintax@obscurepacket.org>
2019-07-30 12:59:18 -0400

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cwf2neo',
-    version='0.1.0',
+    version='0.2.0',
     packages=['cwf2neo'],
     license='Apache License 2.0',
     description='cwf2neo is a Python library use to download, '
