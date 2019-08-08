5fbce2c7eb349dd6d85c0a33d5cb8cac258c0a46
Craig Koroscil <ckoroscil@circadence.com>
2019-08-08 11:02:23 -0400

    updated readme and setup dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+from pathlib import Path
+
 from setuptools import setup
+
+readme = Path(__file__).parent.joinpath('README.md')
+if readme.exists():
+    with readme.open() as f:
+        long_description = f.read()
+else:
+    long_description = '-'
 
 setup(
     name='cwf2neo',
@@ -14,8 +23,6 @@ setup(
         'confuse',
         'progress',
         'py2neo',
-        'xlrd',
-        'bumpversion',
-        'twine'
+        'xlrd'
     ]
 )
