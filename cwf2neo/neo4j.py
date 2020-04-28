9fe173b3777439fab455c3fce91778495e920860
Craig Koroscil <ckoroscil@circadence.com>
2020-04-28 11:33:21 -0400

    fixed environment variable overrides

--- cwf2neo/neo4j.py
+++ cwf2neo/neo4j.py
@@ -9,10 +9,5 @@ log = logging.getLogger(__name__)
 class Neo4j(object):
 
     def __init__(self, **kwargs):
-        if 'NEO4J_HOSTNAME' in os.environ.keys():
-            log.info("Setting Neo4j Hostname to: {}".format(
-                os.environ['NEO4J_HOSTNAME']))
-            kwargs['host'] = os.environ['NEO4J_HOSTNAME']
-        if 'NEO4J_AUTH' in os.environ.keys():
-            kwargs['auth'] = os.environ['NEO4J_AUTH'].split("/")
+        log.debug("Neo4j connection settings: {}".format(kwargs))
         self.graph = Graph(**kwargs)
